b63732413e7bad94361699c31d974817fa89d830
Collins Kiprono <161050774+collolang@users.noreply.github.com>
2026-04-12 21:17:04 +0300

    commit changes

--- src/utils/email.js
+++ src/utils/email.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT || '587'),
-  secure: process.env.EMAIL_PORT, // true for 465, false for other ports
+  port: parseInt(process.env.EMAIL_PORT),
+  secure:process.env.EMAIL_PORT === '465', // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
